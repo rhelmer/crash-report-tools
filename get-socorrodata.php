5e3b991f3cbf68e3895f002f8307b782a338ef47
Robert Kaiser <kairo@kairo.at>
2012-02-17 18:50:54 +0100

    add a script to fetch total crash and ADU data from Socorro /daily CSVs

---
+++ get-socorrodata.php
@@ -0,0 +1,96 @@
+#!/usr/bin/php
+<?php
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// This script retrieves explosiveness stats for crash signatures.
+
+// *** non-commandline handling ***
+
+if (php_sapi_name() != 'cli') {
+  // not commandline, assume apache and output own source
+  header('Content-Type: text/plain; charset=utf8');
+  print(file_get_contents($_SERVER['SCRIPT_FILENAME']));
+  exit;
+}
+
+// *** script settings ***
+
+// turn on error reporting in the script output
+ini_set('display_errors', 1);
+
+// make sure new files are set to -rw-r--r-- permissions
+umask(022);
+
+// set default time zone - right now, always the one the server is in!
+date_default_timezone_set('America/Los_Angeles');
+
+
+// *** data gathering variables ***
+
+// daily reports to gather info from.
+//   product as key, array of versions as value
+
+$daily = array(
+    'Firefox' => array('10.0', '10.0.1', '10.0.2',
+                       '11.0b1', '11.0b2', '11.0b3',
+                       '12.0a2', '13.0a1'),
+    'Fennec' => array('10.0', '10.0.1', '10.0.2',
+                      '11.0b1', '11.0b2', '11.0b3',
+                      '12.0a2', '13.0a1'),
+    'FennecAndroid' => array('11.0b1', '11.0b2', '11.0b3',
+                             '12.0a2', '13.0a1'),
+);
+
+// for how many days back to get the data
+$backlog_days = 20;
+
+// *** URLs and paths ***
+
+$on_moz_server = file_exists('/mnt/crashanalysis/rkaiser/');
+// First %s is product, second is version.
+$url_daily_mask = 'https://crash-stats.mozilla.com/daily?p=%s&v[]=%s&csv=1';
+//https://crash-stats.mozilla.com/daily?p=Firefox&v[]=10.0.1&os[]=Windows&os[]=Mac&os[]=Linux&date_start=2012-02-02&date_end=2012-02-16&form_selection=by_version&csv=1&hang_type=any
+$path_outputbase = $on_moz_server?'':'/home/robert/git-kairo/testbed/socorro/';
+
+if ($on_moz_server) { chdir('/mnt/crashanalysis/rkaiser/'); }
+else { chdir('/mnt/mozilla/projects/socorro/'); }
+
+
+// *** code start ***
+
+// get current day
+$curtime = time();
+
+foreach ($daily as $product=>$versions) {
+  $fproddata = $product.'-daily.json';
+
+  if (file_exists($fproddata)) {
+    print('Read stored '.$product.' daily data'."\n");
+    $proddata = json_decode(file_get_contents($fproddata), true);
+  }
+  else {
+    $proddata = array();
+  }
+
+  foreach ($versions as $ver) {
+    print('Fetch daily data for '.$product.' '.$ver."\n");
+    $dailycsv = file(sprintf($url_daily_mask, $product, $ver));
+    foreach ($dailycsv as $csvline) {
+      $fields = explode(',', $csvline);
+      if (preg_match('/^\d+-\d+-\d+$/', $fields[0])) {
+        $day = $fields[0];
+        $crashes = intval($fields[1]);
+        $adu = intval($fields[2]);
+        if ($crashes && $adu) {
+          $proddata[$ver][$day] = array('crashes' => $crashes,
+                                        'adu' => $adu);
+        }
+      }
+    }
+  }
+  file_put_contents($fproddata, json_encode($proddata));
+}
+
+?>
