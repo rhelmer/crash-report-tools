b14b456d3462aa7e53ccce0abdd0dfcd759c7116
Robert Kaiser <kairo@kairo.at>
2012-03-12 19:44:26 +0100

    also filter android.* into .java

--- get-components.php
+++ get-components.php
@@ -266,6 +266,11 @@ foreach ($reports as $rep) {
           $subfile = '>'.$toplevel.$subfile;
           $toplevel = '.java';
         }
+        // Signatures starting in android. belong into the Java module as well.
+        elseif (preg_match('/^android\./', $sig)) {
+          $subfile = '>'.$toplevel.$subfile;
+          $toplevel = '.java';
+        }
         // Those dirs need to be subcategorized by their direct subdirs.
         $subcat_tls = array('db', 'extensions', 'media', 'modules');
         if (in_array($toplevel, $subcat_tls) && preg_match('/^\/([^\/]+)(\/.*)$/', $subfile, $regs)) {
