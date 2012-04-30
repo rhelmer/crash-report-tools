938df328e92936791793a89845ebc30d942ea0b7
Robert Kaiser <kairo@kairo.at>
2012-04-30 16:00:55 +0200

    update buildcrashes versions

--- get-buildcrashes.php
+++ get-buildcrashes.php
@@ -32,10 +32,10 @@ date_default_timezone_set('America/Los_Angeles');
 // *** data gathering variables ***
 
 // channels
-$channels = array('beta' => '1[23]\.0',
-                  'release' => '1[12]\.0(\.\d)?',
-                  'aurora' => '13\.0a2',
-                  'nightly' => '14\.0a1');
+$channels = array('beta' => '13\.0',
+                  'release' => '11\.0(\.\d)?',
+                  'aurora' => '14\.0a2',
+                  'nightly' => '15\.0a1');
 
 // products
 $products = array('Firefox','Fennec','FennecAndroid');
