66896a7df30166c415d7b7d7f08b33aae7540465
Robert Kaiser <kairo@kairo.at>
2013-01-09 17:40:15 +0100

    add new Flash versions

--- get-flashversions.php
+++ get-flashversions.php
@@ -54,7 +54,9 @@ if (count($force_dates)) {
 $flash_versions = array('11.2.202.235',
                         '11.4.402.287',
                         '11.5.502.135',
-                        '11.6.602.108');
+                        '11.5.502.146',
+                        '11.6.602.108',
+                        '11.6.602.137');
 
 // for how many days back to get the data
 $backlog_days = 7;
