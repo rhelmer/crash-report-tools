2f37e3f2541f5bcde95f9b8cb936a95f14bb9a76
Robert Kaiser <kairo@kairo.at>
2013-03-13 16:01:28 +0100

    add new Patch Tuesday version of Flash

--- get-flashversions.php
+++ get-flashversions.php
@@ -54,6 +54,7 @@ if (count($force_dates)) {
 $flash_versions = array('11.2.202.235',
                         '11.5.502.149',
                         '11.6.602.171',
+                        '11.6.602.180',
                         '11.7.700.128');
 
 // for how many days back to get the data
