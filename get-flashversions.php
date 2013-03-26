138fda67650cac482dafb638288991d3b301940b
Robert Kaiser <kairo@kairo.at>
2013-03-26 03:40:37 +0100

    remove old Flash beta

--- get-flashversions.php
+++ get-flashversions.php
@@ -53,7 +53,6 @@ if (count($force_dates)) {
 
 $flash_versions = array('11.2.202.235',
                         '11.6.602.180',
-                        '11.7.700.141',
                         '11.7.700.146');
 
 // for how many days back to get the data
