ba2015c6698609844ac4ea2524337eb5eb44ee6e
Robert Kaiser <kairo@kairo.at>
2013-04-03 15:59:40 +0200

    more Flash betas

--- get-flashversions.php
+++ get-flashversions.php
@@ -53,8 +53,8 @@ if (count($force_dates)) {
 
 $flash_versions = array('11.2.202.235',
                         '11.6.602.180',
-                        '11.7.700.150',
-                        '11.7.700.165');
+                        '11.7.700.165',
+                        '11.7.700.170');
 
 // for how many days back to get the data
 $backlog_days = 7;
