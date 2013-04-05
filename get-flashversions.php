0a29a714bc723708530aa306f204aaffa6461b8e
Robert Kaiser <kairo@kairo.at>
2013-04-05 16:43:33 +0200

    bump mincounts, remove old falshversion

--- get-flashversions.php
+++ get-flashversions.php
@@ -53,7 +53,6 @@ if (count($force_dates)) {
 
 $flash_versions = array('11.2.202.235',
                         '11.6.602.180',
-                        '11.7.700.165',
                         '11.7.700.170');
 
 // for how many days back to get the data
