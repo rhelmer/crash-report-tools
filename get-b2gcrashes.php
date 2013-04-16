64e720c394a6b5fd745b98c99b79e8b943c7f544
Robert Kaiser <kairo@kairo.at>
2013-04-16 15:57:20 +0200

    use two-argument substring to make this matching easier

--- get-b2gcrashes.php
+++ get-b2gcrashes.php
@@ -98,7 +98,7 @@ for ($daysback = $backlog_days + 1; $daysback > 0; $daysback--) {
   if (!file_exists($anafbcdata)) {
     $rep_query =
       'SELECT version,build,release_channel,'
-      ."SUBSTRING(os_version from '%#\"(otoro|unagi|keon|twist|buri|inari|ikura|hamachi|d300|leo)#\"%' for '#') as device,"
+      ."SUBSTRING(os_version from '(otoro|unagi|keon|twist|buri|inari|ikura|hamachi|d300|leo)') as device,"
       .'process_type,signature,date_processed,uuid '
       .'FROM reports '
       ."WHERE product='B2G' AND os_name='Android' AND utc_day_is(date_processed, '".$anadir."') "
