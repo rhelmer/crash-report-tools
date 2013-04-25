64d47b6132f9a23aa8ec6a01ad296d22e6505f31
Robert Kaiser <kairo@kairo.at>
2013-04-25 15:00:28 +0200

    immediate make reports available of the respective pages once the scripts have run to completion

--- get-flashversions.php
+++ get-flashversions.php
@@ -55,7 +55,7 @@ $flash_versions = array('11.2.202.235',
                         '11.6.602.180',
                         '11.7.700.169',
                         '11.7.700.182',
-                        '11.7.700.186');
+                        '11.7.700.191');
 
 // for how many days back to get the data
 $backlog_days = 7;
