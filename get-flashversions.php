1acc34ec7d79a547925685696a1f77c46f9be7f1
Robert Kaiser <kairo@kairo.at>
2013-06-18 15:28:14 +0200

    remove some old Flash versions

--- get-flashversions.php
+++ get-flashversions.php
@@ -52,9 +52,7 @@ if (count($force_dates)) {
 // Flash versions to gather reports for.
 
 $flash_versions = array('11.2.202.235',
-                        '11.7.700.202',
                         '11.7.700.224',
-                        '11.8.800.75',
                         '11.8.800.81');
 
 // for how many days back to get the data
