ae6114b968967b14d90b5c291ffcededa43e3a93
Robert Kaiser <kairo@kairo.at>
2013-07-18 16:33:06 +0200

    add new Flash (beta?) version

--- get-flashversions.php
+++ get-flashversions.php
@@ -54,7 +54,8 @@ if (count($force_dates)) {
 $flash_versions = array('11.2.202.235',
                         '11.7.700.224',
                         '11.8.800.94',
-                        '11.8.800.95');
+                        '11.8.800.95',
+                        '11.8.800.115');
 
 // for how many days back to get the data
 $backlog_days = 7;
