823406efc483429b6132ef2712e5fe38da68a8d0
Robert Kaiser <kairo@kairo.at>
2013-07-24 14:27:28 +0200

    adjust Flash versions

--- get-flashversions.php
+++ get-flashversions.php
@@ -54,8 +54,8 @@ if (count($force_dates)) {
 $flash_versions = array('11.2.202.235',
                         '11.7.700.224',
                         '11.8.800.94',
-                        '11.8.800.95',
-                        '11.8.800.115');
+                        '11.8.800.115',
+                        '11.8.800.120');
 
 // for how many days back to get the data
 $backlog_days = 7;
