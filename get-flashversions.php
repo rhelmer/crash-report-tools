c6e8621ba0137ea849fd2813f0ef1d52b6b9fb13
Robert Kaiser <kairo@kairo.at>
2014-03-12 16:41:46 +0100

    update for new Flash versions

--- get-flashversions.php
+++ get-flashversions.php
@@ -52,8 +52,9 @@ if (count($force_dates)) {
 // Flash versions to gather reports for.
 
 $flash_versions = array('12.0.0.70',
-                        '13.0.0.133',
-                        '13.0.0.154');
+                        '12.0.0.77',
+                        '13.0.0.154',
+                        '13.0.0.162');
 
 // for how many days back to get the data
 $backlog_days = 7;
