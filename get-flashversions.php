e968e5c156dd73c5aa46b3d3495cd9253b5da69b
Robert Kaiser <kairo@kairo.at>
2014-04-18 16:46:31 +0200

    bump android beta, add some devices, add a Flash beta version

--- get-flashversions.php
+++ get-flashversions.php
@@ -53,7 +53,8 @@ if (count($force_dates)) {
 
 $flash_versions = array('12.0.0.77',
                         '13.0.0.182',
-                        '13.0.0.191');
+                        '13.0.0.191',
+                        '13.0.0.199');
 
 // for how many days back to get the data
 $backlog_days = 7;
