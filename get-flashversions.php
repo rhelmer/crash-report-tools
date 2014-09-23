2063b40df3a154f4468ab6e60a9434ae8267b991
Robert Kaiser <kairo@kairo.at>
2014-09-23 16:06:41 +0200

    some mincount bumps and removing old Flash versions

--- get-flashversions.php
+++ get-flashversions.php
@@ -51,10 +51,8 @@ if (count($force_dates)) {
 
 // Flash versions to gather reports for.
 
-$flash_versions = array('14.0.0.145',
-                        '14.0.0.179',
-                        '15.0.0.152',
-                        '15.0.0.159');
+$flash_versions = array('15.0.0.152',
+                        '15.0.0.170');
 
 // for how many days back to get the data
 $backlog_days = 7;
